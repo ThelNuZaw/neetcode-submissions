9fe8ed260e7db52f7cd5e53f54a9b7f8cb3eaf7b
Thel Nu Zaw <trixiethelnu@gmail.com>
2026-08-31 15:02:35 -0700

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        MIN = pow(-2, 31)
+        MAX = pow(2, 31) - 1
+        res = 0
+        while x:
+            last_digit = int(math.fmod(x, 10))
+            x = int(x / 10)
+            if (res > MAX // 10 or (res == MAX // 10 and last_digit >= MAX % 10 )):
+                return 0
+            if (res < MIN // 10 or (res == MIN // 10 and last_digit <= MIN % 10 )):
+                return 0
+
+            res = (res * 10) + last_digit
+        return res
